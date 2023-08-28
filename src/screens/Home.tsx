df9a3ce93d3a55541f1d6eba95fbeadd35e2e7f4
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-08-28 08:27:10 -0300

    feat: :sparkles: implementa componentes de grupos e cards de exercício e finaliza estilização da home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,64 @@
+import { useState } from 'react'
+import { FlatList, Heading, HStack, Text, VStack } from 'native-base'
+
+import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { Text, VStack } from 'native-base'
+import { ExerciseCard } from '@components/ExerciseCard'
 
 export function Home() {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro'])
+  const [exercises, setExercises] = useState([
+    'Puxada frontal',
+    'Remada curvada',
+    'Remada unilateral',
+    'Levantamento terras',
+  ])
+  const [groupSelected, setGroupSelected] = useState('Costas')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 20,
+          }}
+        />
+      </VStack>
     </VStack>
   )
 }
