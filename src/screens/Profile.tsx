9bb0c527217ecd1b5a5f5d593fb83fa18027ce76
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-08-28 09:06:55 -0300

    feat: :sparkles: implementa tela de perfil contendo foto e inputs nome, email e senha

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,86 @@
-import { Center, Text } from 'native-base'
+import { useState } from 'react'
+import { TouchableOpacity } from 'react-native'
+import {
+  Center,
+  ScrollView,
+  VStack,
+  Skeleton,
+  Text,
+  Heading,
+  useTheme,
+} from 'native-base'
+
+import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+import { Input } from '@components/Input'
+import { Button } from '@components/Button'
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
-    <Center flex={1}>
-      <Text color="white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/raniellimontagna.png' }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input bg="gray.600" placeholder="Nome" />
+
+          <Input bg="gray.600" placeholder="E-mail" isDisabled />
+
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            alignSelf="flex-start"
+            mt={12}
+          >
+            Alterar senha
+          </Heading>
+
+          <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+
+          <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+
+          <Input
+            bg="gray.600"
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+
+          <Button title="Atualizar" mt={4} />
+        </Center>
+      </ScrollView>
+    </VStack>
   )
 }
