1ed8fa5931d0c177d9730adf1b16908e20a51b68
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-08-28 08:33:42 -0300

    feat: :sparkles: implementa novos componentes e utilizas section list finalizando estilizaçao da tela `history`

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -25,7 +25,9 @@ export function Home() {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={groupSelected === item}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
             onPress={() => setGroupSelected(item)}
           />
         )}
