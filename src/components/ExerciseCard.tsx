519fe512679c501c9c09475fddee9dd1d4611331
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-11-11 13:05:07 -0300

    feat: :sparkles: implementa formulários utilizando `react-hook-form`

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,25 +1,18 @@
-import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
-import { Heading, HStack, Image, Text, VStack, Icon } from 'native-base'
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { Heading, HStack, Image, Text, VStack, Icon } from 'native-base';
 
-import { Entypo } from '@expo/vector-icons'
+import { Entypo } from '@expo/vector-icons';
 
-type Props = TouchableOpacityProps & {}
+type Props = TouchableOpacityProps & {
+  
+};
 
 export function ExerciseCard({ ...rest }: Props) {
   return (
     <TouchableOpacity {...rest}>
-      <HStack
-        bg="gray.500"
-        alignItems="center"
-        p={2}
-        pr={4}
-        rounded="md"
-        mb={3}
-      >
-        <Image
-          source={{
-            uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg',
-          }}
+      <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
+        <Image 
+          source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
           alt="Imagem do exercício"
           w={16}
           h={16}
@@ -29,7 +22,7 @@ export function ExerciseCard({ ...rest }: Props) {
         />
 
         <VStack flex={1}>
-          <Heading fontSize="lg" color="white">
+          <Heading fontSize="lg" color="white" fontFamily="heading">
             Remanda unilateral
           </Heading>
 
@@ -38,8 +31,12 @@ export function ExerciseCard({ ...rest }: Props) {
           </Text>
         </VStack>
 
-        <Icon as={Entypo} name="chevron-thin-right" color="gray.300" />
+        <Icon 
+          as={Entypo}
+          name="chevron-thin-right"
+          color="gray.300"
+        />
       </HStack>
     </TouchableOpacity>
-  )
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,20 @@
-import { useTheme, Box } from 'native-base'
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { useTheme, Box } from 'native-base';
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-import { AuthRoutes } from './auth.routes'
-import { AppRoutes } from './app.routes'
+import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
-  const { colors } = useTheme()
+  const { colors } = useTheme();
 
-  const theme = DefaultTheme
-  theme.colors.background = colors.gray[700]
+  const theme = DefaultTheme;
+  theme.colors.background = colors.gray[700];
 
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AppRoutes />
+        <AuthRoutes />
       </NavigationContainer>
     </Box>
-  )
+  );
 }
