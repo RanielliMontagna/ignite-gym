8330fa269f042d5503e60a59e7b16bde20b441d2
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-11-11 14:14:43 -0300

    feat: :sparkles: implementa `axios` e tratamento de erro básico

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,49 +1,90 @@
-import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
-import { useForm, Controller } from 'react-hook-form';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { useNavigation } from '@react-navigation/native'
+import {
+  VStack,
+  Image,
+  Text,
+  Center,
+  Heading,
+  ScrollView,
+  useToast,
+} from 'native-base'
+import { useForm, Controller } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
-import LogoSvg from '@assets/logo.svg';
-import BackgroundImg from '@assets/background.png';
+import { api } from '@services/api'
 
-import { Input } from "@components/Input";
-import { Button } from "@components/Button";
+import LogoSvg from '@assets/logo.svg'
+import BackgroundImg from '@assets/background.png'
+
+import { AppError } from '@utils/AppError'
+
+import { Input } from '@components/Input'
+import { Button } from '@components/Button'
 
 type FormDataProps = {
-  name: string;
-  email: string;
-  password: string;
-  password_confirm: string;
+  name: string
+  email: string
+  password: string
+  password_confirm: string
 }
 
 const signUpSchema = yup.object({
   name: yup.string().required('Informe o nome.'),
   email: yup.string().required('Informe o e-mail').email('E-mail inválido.'),
-  password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 dígitos.'),
-  password_confirm: yup.string().required('Confirme a senha.').oneOf([yup.ref('password'), null], 'A confirmação da senha não confere')
-});
+  password: yup
+    .string()
+    .required('Informe a senha')
+    .min(6, 'A senha deve ter pelo menos 6 dígitos.'),
+  password_confirm: yup
+    .string()
+    .required('Confirme a senha.')
+    .oneOf([yup.ref('password'), null], 'A confirmação da senha não confere'),
+})
 
 export function SignUp() {
-  
-  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
-    resolver: yupResolver(signUpSchema),
-  });
+  const toast = useToast()
 
-  const navigation = useNavigation();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema),
+  })
+
+  const navigation = useNavigation()
 
   function handleGoBack() {
-    navigation.goBack();
+    navigation.goBack()
   }
 
-  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-    console.log({ name, email, password, password_confirm })
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    try {
+      const response = await api.post('/users', { name, email, password })
+      console.log(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível criar a conta. Tente novamente mais tarde'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    }
   }
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-        <VStack flex={1} px={10} pb={16}>
-        <Image 
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} px={10} pb={16}>
+        <Image
           source={BackgroundImg}
           defaultSource={BackgroundImg}
           alt="Pessoas treinando"
@@ -64,11 +105,11 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Controller 
+          <Controller
             control={control}
             name="name"
             render={({ field: { onChange, value } }) => (
-              <Input 
+              <Input
                 placeholder="Nome"
                 onChangeText={onChange}
                 value={value}
@@ -77,12 +118,12 @@ export function SignUp() {
             )}
           />
 
-          <Controller 
+          <Controller
             control={control}
             name="email"
             render={({ field: { onChange, value } }) => (
-              <Input 
-                placeholder="E-mail" 
+              <Input
+                placeholder="E-mail"
                 keyboardType="email-address"
                 autoCapitalize="none"
                 onChangeText={onChange}
@@ -91,13 +132,13 @@ export function SignUp() {
               />
             )}
           />
-          
-          <Controller 
+
+          <Controller
             control={control}
             name="password"
             render={({ field: { onChange, value } }) => (
-              <Input 
-                placeholder="Senha" 
+              <Input
+                placeholder="Senha"
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
@@ -106,12 +147,12 @@ export function SignUp() {
             )}
           />
 
-          <Controller 
+          <Controller
             control={control}
             name="password_confirm"
             render={({ field: { onChange, value } }) => (
-              <Input 
-                placeholder="Confirmar a Senha" 
+              <Input
+                placeholder="Confirmar a Senha"
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
@@ -122,19 +163,19 @@ export function SignUp() {
             )}
           />
 
-          <Button 
-            title="Criar e acessar" 
+          <Button
+            title="Criar e acessar"
             onPress={handleSubmit(handleSignUp)}
           />
         </Center>
-        
-        <Button 
-          title="Voltar para o login" 
-          variant="outline" 
+
+        <Button
+          title="Voltar para o login"
+          variant="outline"
           mt={12}
           onPress={handleGoBack}
         />
       </VStack>
     </ScrollView>
-  );
+  )
 }
